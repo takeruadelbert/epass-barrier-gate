4fa36aeeb90fbe755ed5a4ee9ffafd0adb5c5c86
Lukas Fam <takeruadelbert@gmail.com>
2018-11-15 17:16:48 +0700

    added an error handling when input device is not readable

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 from src.HID import *
 
+from os import access, R_OK
+
 if __name__ == "__main__" :
-    hid = HID()
-    hid.read_input()
+    # check if HID is readable
+    if access(hid_path, R_OK) :
+        hid = HID()
+        hid.read_input()
+    else :
+        err_message = "File '{}' isn't readable".format(hid_path)
+        print(err_message)
